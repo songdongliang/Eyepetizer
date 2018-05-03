be47e7cdbdf1082d27b8e0a643e0d22c1d09e005
songdongliang <sdl_itperson@outlook.com>
2018-05-03 22:45:46 +0800

    keep code

--- app/src/main/java/com/sdl/eyepetizer/adapter/HomeAdapter.kt
+++ app/src/main/java/com/sdl/eyepetizer/adapter/HomeAdapter.kt
@@ -1,10 +1,12 @@
 package com.sdl.eyepetizer.adapter
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.sdl.eyepetizer.R
 import com.sdl.eyepetizer.model.HomeBean
 
 class HomeAdapter(): RecyclerView.Adapter<BindingViewHolder<ViewDataBinding>>() {
@@ -19,7 +21,7 @@ class HomeAdapter(): RecyclerView.Adapter<BindingViewHolder<ViewDataBinding>>() 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingViewHolder<ViewDataBinding> {
-
+        return BindingViewHolder(DataBindingUtil.inflate(inflater!!, R.layout.item_home,parent,false))
     }
 
     override fun getItemCount(): Int {
